fe2dbab55bc3cd4d2e0d8a77e8f9ab19deadad1c
Juan Cruz <47389717+juanlti@users.noreply.github.com>
2024-09-24 20:29:47 -0300

    cada hilo debe obtener un rango de operacion antes de realizar la suma.java

--- ejer6/Sumadora.java
+++ ejer6/Sumadora.java
@@ -3,32 +3,34 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tp3.ejer6;
+package tp3.Ejercicio6;
 
 /**
  *
- * @author juan.barrera
+ * @author juanc
  */
 public class Sumadora extends Thread {
 
-    Numeros  nros;
-    int pos,ultimaPosicion;
+    Numeros nros;
+    int pos, ultimaPosicion;
     int nombre;
 
-    public Sumadora(int posicion,Numeros n, int nombreHilo) {
+    public Sumadora(int posicion, Numeros n, int nombreHilo) {
         this.nros = n;
         this.pos = posicion;
         this.nombre = nombreHilo;
-        ultimaPosicion=0;
 
     }
 
-
     public void run() {
-//indiceDeHilo, int nombreHilo, int ultimaPosicion
-            int total = this.nros.sumaLocal(pos, nombre,ultimaPosicion);
-        
-            this.nros.sumar(total);
+
+        //asignacion
+        ultimaPosicion = this.nros.asignacionPorHilo();
+
+        //indiceDeHilo, int nombreHilo, int ultimaPosicion
+        int total = this.nros.sumaLocal(5, nombre, ultimaPosicion);
+
+        this.nros.sumar(total);
 
     }
 }

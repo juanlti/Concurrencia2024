f24ee07c6ac8fcab434cd4cabf38217b55447925
Juan Cruz <47389717+juanlti@users.noreply.github.com>
2024-10-07 00:24:16 -0300

    Update Test.java

--- tp4/Ejercicio5/Test.java
+++ tp4/Ejercicio5/Test.java
@@ -16,11 +16,11 @@ public class Test {
 
     public static void main(String[] args) {
 
-        Impresion A1 = new Impresion('A', 53);
-        Impresion A2 = new Impresion('A', 35);
-        Impresion B = new Impresion('B', 45);
+        Impresion A1 = new Impresion("A1", 53);
+        Impresion A2 = new Impresion("A2", 35);
+        Impresion B = new Impresion("B", 45);
         //GestorImpresoras gestorImpresora = new GestorImpresoras(A, B);
-        //    public Usuario(GestorImpresoras gestorImpresora, char unTipoDoc, int cantHojasMas) {
+        //public Usuario(GestorImpresoras gestorImpresora, char unTipoDoc, int cantHojasMas) {
 
         GestorImpresoras gestor = new GestorImpresoras(A1, A2, B);
 
@@ -29,15 +29,15 @@ public class Test {
         Usuario[] usuariosB = new Usuario[3];
         Usuario[] usuariosX = new Usuario[2];
         for (int i = 0; i < usuariosA.length; i++) {
-            usuariosA[i] = new Usuario(gestor, 'A', 10);
+            usuariosA[i] = new Usuario(gestor, "A", i, 10);
 
         }
         for (int i = 0; i < usuariosB.length; i++) {
-            usuariosB[i] = new Usuario(gestor, 'B', 10);
+            usuariosB[i] = new Usuario(gestor, "B", i, 10);
 
         }
         for (int i = 0; i < usuariosX.length; i++) {
-            usuariosX[i] = new Usuario(gestor, 'X', 10);
+            usuariosX[i] = new Usuario(gestor, "X", i, 10);
 
         }
 
@@ -54,7 +54,7 @@ public class Test {
             usuariosX[i].start();
 
         }
-        System.out.println("algo    ");
+
         // join
         for (int i = 0; i < usuariosA.length; i++) {
             try {
@@ -81,6 +81,7 @@ public class Test {
             }
 
         }
+        gestor.disponibilidadRestante();
 
     }
 

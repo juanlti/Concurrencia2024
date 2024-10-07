ae89505c2a2d07ae7263f2b36e209efc49387105
Juan Cruz <47389717+juanlti@users.noreply.github.com>
2024-10-07 00:24:28 -0300

    Update Usuario.java

--- tp4/Ejercicio5/Usuario.java
+++ tp4/Ejercicio5/Usuario.java
@@ -5,29 +5,44 @@
  */
 package tp4.Ejercicio5;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author juanc
  */
 public class Usuario extends Thread {
 
-    char tipoDcomento;
+    String tipoDcomento;
     GestorImpresoras gestorImpresora;
-    int cantHojas;
+    int cantHojas, id;
 
-    public Usuario(GestorImpresoras gestorImpresora, char unTipoDoc, int cantHojasMas) {
+    public Usuario(GestorImpresoras gestorImpresora, String unTipoDoc, int id, int cantHojasMas) {
         this.gestorImpresora = gestorImpresora;
         this.tipoDcomento = unTipoDoc;
         this.cantHojas = cantHojasMas;
+        this.id = id;
 
     }
 
     public void run() {
-        int res = 5;
-        while (res == 5) {
-            res = this.gestorImpresora.derivarImpresion(this.cantHojas, this.tipoDcomento);
+        int res = 2;
+
+        int i = 0;
+        while ((res != 1 && res != 0) && i < 50) {
+            try {
+                res = this.gestorImpresora.derivarImpresion(this.cantHojas, this.tipoDcomento, this.id);
+
+                Thread.sleep(200);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("RES " + res + " i " + i);
+            i++;
 
         }
+        System.out.println("Salio " + id + " valor de salida " + res);
 
     }
 

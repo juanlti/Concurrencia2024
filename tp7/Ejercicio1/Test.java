878f9a1416fedc2b3d0f922d3c50e243f5f63071
Juan Cruz <47389717+juanlti@users.noreply.github.com>
2024-10-24 23:51:21 -0300

    Update Test.java

--- tp7/Ejercicio1/Test.java
+++ tp7/Ejercicio1/Test.java
@@ -4,11 +4,28 @@
  * and open the template in the editor.
  */
 package tp7.Ejercicio1;
-
-/**
- *
- * @author juanc
- */
 public class Test {
-    
-}
+
+    public static void main(String[] args) {
+
+        Puente puente = new Puente();
+        CocheNorte[] cn = new CocheNorte[10];
+        CocheSur[] sr = new CocheSur[25];
+
+        for (int i = 0; i < cn.length; i++) {
+            cn[i] = new CocheNorte(puente);
+
+        }
+        for (int i = 0; i < sr.length; i++) {
+            sr[i] = new CocheSur(puente);
+        }
+
+        for (int i = 0; i < cn.length; i++) {
+            cn[i].start();
+        }
+        for (int i = 0; i < sr.length; i++) {
+            sr[i].start();
+        }
+        
+        
+    }
